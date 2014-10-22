e134081b14ad9b221474cbd75720b5f1ca83f80f
Calvin <amcalvinw@gmail.com>
2014-10-22 01:09:30 -0400

    a

--- show.php
+++ show.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET['employer']; ?>
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST['employer']; ?>
 
 </body>
 </html>
