63dc97d8962a94ef0c87f9294cddd9d5931d0427
Calvin <amcalvinw@gmail.com>
2014-10-26 15:18:24 -0400

    delete free lancers

--- lab8.php
+++ lab8.php
@@ -3,7 +3,6 @@
     <meta charset="utf-8">
 	<title>Lab8 New Job Post</title>
 	<!-- Custom CSS -->
-    <link href="css/freelancer.css" rel="stylesheet">
 	<!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom Fonts -->
