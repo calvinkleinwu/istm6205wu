f2d25083f3be9dc8dc5ff2eb35082114d675d127
Calvin <amcalvinw@gmail.com>
2014-10-26 16:00:30 -0400

    css path change

--- lab8.php
+++ lab8.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
 	<title>Lab8 New Job Post</title>
 	<!-- Custom CSS -->
-    <link rel="stylesheet" type="text/css" href="lab8.css">
+    <link rel="stylesheet" type="text/css" href="css/lab8.css">
 	<!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom Fonts -->
